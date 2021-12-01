082116de22a074213d904fb8e55912903067d09f
Tom VanAntwerp <tom@tomvanantwerp.com>
2021-12-01 11:22:20 -0500

    Solution to day one puzzle one 2021

--- 2021/day-01/puzzle-01/index.js
+++ 2021/day-01/puzzle-01/index.js
@@ -1,1 +1,22 @@
-// First Puzzle
+const fs = require('fs');
+const path = require('path');
+
+fs.readFile(path.join(__dirname, './input.txt'), 'utf8', (err, data) => {
+	if (err) {
+		console.error(err);
+		return;
+	}
+
+	const input = data.split('\n').map(d => +d);
+	let count = 0;
+	console.log(input);
+	input.forEach((measure, index) => {
+		if (index > 0) {
+			if (measure > input[index - 1]) {
+				count += 1;
+			}
+		}
+	});
+
+	console.log(count);
+});
