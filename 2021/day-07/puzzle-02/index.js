4facb847dd0b51aad22f53f3687407675ba9b694
Tom VanAntwerp <tom@tomvanantwerp.com>
2021-12-07 12:47:38 -0500

    Solve day seven puzzle two 2021

---
+++ 2021/day-07/puzzle-02/index.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readFile(path.join(__dirname, '../input.txt'), 'utf8', (err, data) => {
+	if (err) {
+		console.error(err);
+		return;
+	}
+
+	const input = data.split(',').map(d => +d);
+	const theAverage = average(input);
+
+	let fuelToAverage = 0;
+	input.forEach(value => {
+		const distance = Math.abs(value - theAverage);
+		// triangular number sequence
+		let fuel = triangularNumber(distance);
+		fuelToAverage += fuel;
+	});
+
+	console.log('using average (broken)', fuelToAverage);
+
+	const positions = new Map();
+	const min = Math.min(...input);
+	const max = Math.max(...input);
+	for (let i = min; i <= max; i++) {
+		positions.set(i, 0);
+	}
+	positions.forEach((_, position) => {
+		let totalFuel = 0;
+		input.forEach(crab => {
+			totalFuel += triangularNumber(Math.abs(position - crab));
+		});
+		positions.set(position, totalFuel);
+	});
+	console.log(
+		'using brute force (correct)',
+		Array.from(positions.values()).reduce((prev, curr) =>
+			curr < prev ? curr : prev,
+		),
+	);
+});
+
+function average(arr) {
+	return Math.round(arr.reduce((prev, curr) => prev + curr) / arr.length);
+}
+
+function triangularNumber(distance) {
+	return (distance * (distance + 1)) / 2;
+}
