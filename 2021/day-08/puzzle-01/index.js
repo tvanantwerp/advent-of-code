b4123694361ca7b8a287bd6165c6768b38a1a067
Tom VanAntwerp <tom@tomvanantwerp.com>
2021-12-08 08:59:48 -0500

    Solve day eight puzzle one 2021

--- 2021/day-08/puzzle-01/index.js
+++ 2021/day-08/puzzle-01/index.js
@@ -11,5 +11,13 @@ fs.readFile(path.join(__dirname, '../input.txt'), 'utf8', (err, data) => {
 		.split('\n')
 		.map(d => d.split(' | '))
 		.map(d => d.map(dd => dd.split(' ')));
-	console.log(input);
+
+	let countOfUniqueDigits = 0;
+	input.forEach(d => {
+		d[1].forEach(dd => {
+			if (dd.length == 2 || dd.length == 4 || dd.length == 3 || dd.length == 7)
+				countOfUniqueDigits += 1;
+		});
+	});
+	console.log(countOfUniqueDigits);
 });
