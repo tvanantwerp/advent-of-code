51e0ca1b40c2657cbc6a89a3fc6f63a26ae03f90
Tom VanAntwerp <tom@tomvanantwerp.com>
2021-12-13 10:01:15 -0500

    Set up day thirteen

---
+++ 2021/day-13/puzzle-01/index.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readFile(path.join(__dirname, '../testInput.txt'), 'utf8', (err, data) => {
+	if (err) {
+		console.error(err);
+		return;
+	}
+
+	const input = data.split('\n\n');
+	const coordinates = input[0].split('\n').map(c => c.split(',').map(d => +d));
+	const folds = input[1].split('\n').map(f => {
+		const pattern = /fold along (x|y)=(\d+)/;
+		const match = f.match(pattern);
+		return [match[1], +match[2]];
+	});
+
+	const maxX = Math.max(...coordinates.map(c => c[0]));
+	const maxY = Math.max(...coordinates.map(c => c[1]));
+
+	const defaultRow = Array.from({ length: maxX + 1 }, () => '░');
+	const grid = Array.from({ length: maxY + 1 }, () => [...defaultRow]);
+	coordinates.forEach(coordinate => {
+		grid[coordinate[1]][coordinate[0]] = '█';
+	});
+});
+
+function flipX(grid, axis) {}
+
+function flipY(grid, axis) {
+	const topHalf = grid.slice(0, axis);
+	const bottomHalf = grid.slice(axis + 1).reverse();
+	const foldedGrid = topHalf.map(row => {
+		return row.map((column, i) => {
+			return;
+		});
+	});
+}
+
+function renderGrid(grid) {
+	let output = '';
+	grid.forEach(row => {
+		row.forEach(column => {
+			output += column;
+		});
+		output += '\n';
+	});
+	return output;
+}
