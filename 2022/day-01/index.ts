5be6a398e5a0f0670f647f6523648ab07800243f
Tom VanAntwerp <tom@tomvanantwerp.com>
2022-12-01 09:40:53 -0500

    Clean up day 1 solutions

--- 2022/day-01/index.ts
+++ 2022/day-01/index.ts
@@ -1,18 +1,13 @@
 const input = await Deno.readTextFile("./input.txt");
 
 const part1 = (input: string) => {
-	const elfCalorieTotals = input.split(/\n\n/);
+	const calorieCounts = input.split(/\n\n/).map((calories) =>
+		calories.split("\n").map((d) => +d).reduce((acc, curr) => acc + curr, 0)
+	);
 	let maxCalories = 0;
-	for (let i = 0; i < elfCalorieTotals.length; i++) {
-		const calorieCounts = elfCalorieTotals[i].split("\n").map((d) => +d);
-		const total = calorieCounts.reduce((acc, curr) => {
-			return acc + curr;
-		}, 0);
-		if (total > maxCalories) {
-			console.log(
-				`Old total was ${maxCalories}, new total is ${total} at elf ${i}`,
-			);
-			maxCalories = total;
+	for (let i = 0; i < calorieCounts.length; i++) {
+		if (calorieCounts[i] > maxCalories) {
+			maxCalories = calorieCounts[i];
 		}
 	}
 
@@ -20,8 +15,7 @@ const part1 = (input: string) => {
 };
 
 const part2 = (input: string) => {
-	const elfCalorieTotals = input.split(/\n\n/);
-	const calorieCounts = elfCalorieTotals.map((calories) =>
+	const calorieCounts = input.split(/\n\n/).map((calories) =>
 		calories.split("\n").map((d) => +d).reduce((acc, curr) => acc + curr, 0)
 	);
 
