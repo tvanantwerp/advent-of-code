24c91d2676d095ff091f7c7dc041c02167d6e2b6
Tom VanAntwerp <tom@tomvanantwerp.com>
2022-12-02 21:47:52 -0500

    Add 2015 day 1

---
+++ 2015/day-01/index.ts
@@ -0,0 +1,57 @@
+const __dirname = new URL('.', import.meta.url).pathname;
+const input = await Deno.readTextFile(`${__dirname}input.txt`);
+
+const parseInput = (input: string) => {
+	return input.trim().split('');
+};
+
+const part1 = (input: string) => {
+	const instructions = parseInput(input);
+	let floor = 0;
+	for (let i = 0; i < instructions.length; i++) {
+		if (instructions[i] === '(') floor += 1;
+		if (instructions[i] === ')') floor -= 1;
+		if (instructions[i] !== '(' && instructions[i] !== ')') {
+			throw new Error(
+				`Invalid floor marker, received ${instructions[i]} at ${i}`,
+			);
+		}
+	}
+	return floor;
+};
+
+const part2 = (input: string) => {
+	const instructions = parseInput(input);
+	let floor = 0;
+	for (let i = 0; i < instructions.length; i++) {
+		if (instructions[i] !== '(' && instructions[i] !== ')') {
+			throw new Error(
+				`Invalid floor marker, received ${instructions[i]} at ${i}`,
+			);
+		}
+		if (instructions[i] === '(') floor += 1;
+		if (instructions[i] === ')') floor -= 1;
+		if (floor < 0) {
+			return i + 1;
+		}
+	}
+};
+
+console.log(`Input (()) should be 0, is ${part1('(())')}`);
+console.log(`Input ()() should be 0, is ${part1('()()')}`);
+console.log(`Input ((( should be 3, is ${part1('(((')}`);
+console.log(`Input (()(()( should be 3, is ${part1('(()(()(')}`);
+console.log(`Input ))((((( should be 3, is ${part1('))(((((')}`);
+console.log(`Input ()) should be -1, is ${part1('())')}`);
+console.log(`Input ))( should be -1, is ${part1('))(')}`);
+console.log(`Input ))) should be -3, is ${part1(')))')}`);
+console.log(`Input )())()) should be -3, is ${part1(')())())')}`);
+
+await Deno.writeTextFile(
+	`${__dirname}output1.txt`,
+	JSON.stringify(part1(input)),
+);
+await Deno.writeTextFile(
+	`${__dirname}output2.txt`,
+	JSON.stringify(part2(input)),
+);
