e17c534e742eb16b82c8dc4d7d2df9f18eb12db6
Tom VanAntwerp <tom@tomvanantwerp.com>
2022-12-05 13:42:28 -0500

    Fix bug in input parsing for stacks

--- 2022/day-05/index.ts
+++ 2022/day-05/index.ts
@@ -3,7 +3,7 @@ const test = await Deno.readTextFile(`${__dirname}test.txt`);
 const input = await Deno.readTextFile(`${__dirname}input.txt`);
 
 const parseInput = (input: string) => {
-	const [initialState, initialActions] = input.trim().split('\n\n');
+	const [initialState, initialActions] = input.split('\n\n');
 	const initialStateRows = initialState.split('\n');
 	const stackHeight = initialStateRows.length - 1;
 	const stackCountMatch = initialStateRows[initialStateRows.length - 1].match(
@@ -12,6 +12,7 @@ const parseInput = (input: string) => {
 	const stackCount = parseInt(
 		stackCountMatch[1],
 	);
+	console.table({ stackCount, stackHeight });
 	const stacks: Record<string, string[]> = {};
 	for (let i = 1; i < stackCount + 1; i++) {
 		stacks[i] = [];
@@ -19,13 +20,11 @@ const parseInput = (input: string) => {
 	const cratePattern = /\[(\w)\]\s?/g;
 	let matches: RegExpExecArray | null;
 	for (let i = 0; i < stackHeight; i++) {
+		console.log(initialStateRows[i]);
 		while (matches = cratePattern.exec(initialStateRows[i])) {
 			stacks[1 + matches.index / 4].push(matches[1]);
 		}
 	}
-	Object.keys(stacks).forEach((key) => {
-		stacks[key] = stacks[key].reverse();
-	});
 
 	const actions = initialActions.split('\n').map((action) => {
 		const digits = action.match(/move (\d+) from (\d+) to (\d+)/)!;
@@ -44,14 +43,13 @@ parseInput(test);
 const part1 = (input: string) => {
 	const { stacks, actions } = parseInput(input);
 	for (const action of actions) {
-		const movedCrates = stacks[action.source].splice(0, action.count).reverse();
+		const movedCrates = stacks[action.source].splice(0, action.count);
 		stacks[action.destination].push(...movedCrates);
 	}
 	let topCrates = '';
 	Object.keys(stacks).forEach((key) => {
 		topCrates += stacks[key].at(-1);
 	});
-	console.log(stacks);
 	return topCrates;
 };
 
@@ -69,5 +67,5 @@ console.assert('CMZ' === test1, {
 // 	received: test2,
 // });
 
-console.log(`Part 1 answer: ${part1(input)}`);
+// console.log(`Part 1 answer: ${part1(input)}`);
 // console.log(`Part 2 answer: ${part2(input)}`);
