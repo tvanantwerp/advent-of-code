466cc1b056caa1bbc319e70111ffe45f7bc4d069
Tom VanAntwerp <tom@tomvanantwerp.com>
2022-12-08 13:44:15 -0500

    Refactor 2022 day 1

--- 2022/day-01/index.ts
+++ 2022/day-01/index.ts
@@ -1,29 +1,37 @@
 const __dirname = new URL('.', import.meta.url).pathname;
 const input = await Deno.readTextFile(`${__dirname}input.txt`);
 
-const part1 = (input: string) => {
-	const calorieCounts = input.split(/\n\n/).map((calories) =>
+function parseInput(input: string) {
+	return input.split(/\n\n/).map((calories) =>
 		calories.split('\n').map((d) => +d).reduce((acc, curr) => acc + curr, 0)
 	);
-	let maxCalories = 0;
-	for (let i = 0; i < calorieCounts.length; i++) {
-		if (calorieCounts[i] > maxCalories) {
-			maxCalories = calorieCounts[i];
-		}
-	}
+}
 
-	return maxCalories;
+const part1 = (input: string) => {
+	return Math.max(
+		...parseInput(input),
+	);
 };
 
 const part2 = (input: string) => {
-	const calorieCounts = input.split(/\n\n/).map((calories) =>
-		calories.split('\n').map((d) => +d).reduce((acc, curr) => acc + curr, 0)
-	);
+	const calorieCounts = parseInput(input);
 
-	return calorieCounts.sort((a, b) => b - a).slice(0, 3).reduce(
-		(acc, curr) => acc + curr,
-		0,
-	);
+	let first = 0, second = 0, third = 0;
+	calorieCounts.forEach((count) => {
+		if (count > first) {
+			third = second;
+			second = first;
+			first = count;
+		} else if (count > second) {
+			third = second;
+			second = count;
+		} else if (count > third) third = count;
+	});
+	// return calorieCounts.sort((a, b) => b - a).slice(0, 3).reduce(
+	// 	(acc, curr) => acc + curr,
+	// 	0,
+	// );
+	return first + second + third;
 };
 
 console.log(`Part 1 answer: ${part1(input)}`);
