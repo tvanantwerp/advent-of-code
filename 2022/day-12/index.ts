9368c0095f89df089a827e2e3f34c118d28db0ff
Tom VanAntwerp <tom@tomvanantwerp.com>
2022-12-12 19:38:37 -0500

    Cleanup for publishing

--- 2022/day-12/index.ts
+++ 2022/day-12/index.ts
@@ -10,26 +10,23 @@ function parseInput(
 	start: [number, number];
 	end: [number, number];
 	grid: number[][];
-	aPoints: [number, number][];
 } {
 	let start: Cell = [0, 0], end: Cell = [0, 0];
-	let aPoints: [number, number][] = [];
 	const grid = input.trim().split('\n').map((row, i) => {
 		return row.split('').map((col, j) => {
-			if (col === 'S') {
-				start = [i, j];
-				col = 'a';
-			}
 			if (col === 'E') {
-				end = [i, j];
+				start = [i, j];
 				col = 'z';
 			}
-			if (col === 'a') aPoints.push([i, j]);
+			if (col === 'S') {
+				end = [i, j];
+				col = 'a';
+			}
 			return col.charCodeAt(0) - 'a'.charCodeAt(0);
 		});
 	});
 
-	return { start, end, grid, aPoints };
+	return { start, end, grid };
 }
 
 function getCoordinateWithinBounds(row: number, col: number, grid: number[][]) {
@@ -46,7 +43,7 @@ function cellToString(cell: Cell): string {
 	return `r${cell[0]},c${cell[1]}`;
 }
 
-function bfs(start: Cell, end: Cell, grid: number[][]): number {
+function bfs(start: Cell, end: Cell | number, grid: number[][]): number {
 	const directions = [
 		[0, 1],
 		[0, -1],
@@ -60,63 +57,36 @@ function bfs(start: Cell, end: Cell, grid: number[][]): number {
 	while (queue.length > 0) {
 		const [cellText, steps] = queue.shift()!;
 		const [row, col] = stringToCell(cellText);
+
 		if (visited.has(cellText)) continue;
-		if (row === end[0] && col === end[1]) {
-			return steps;
-		}
+		else visited.add(cellText);
+
+		if (
+			(typeof end === 'number' && grid[row][col] === end) ||
+			(typeof end !== 'number' && row === end[0] && col === end[1])
+		) return steps;
+
 		for (const [y, x] of directions) {
 			if (
-				!getCoordinateWithinBounds(row + y, col + x, grid) ||
-				grid[row + y][col + x] > 1 + grid[row][col] ||
-				visited.has(cellToString([row + y, col + x]))
-			) continue;
-			queue.push([cellToString([row + y, col + x]), steps + 1]);
+				getCoordinateWithinBounds(row + y, col + x, grid) &&
+				grid[row + y][col + x] >= grid[row][col] - 1 &&
+				!visited.has(cellToString([row + y, col + x]))
+			) queue.push([cellToString([row + y, col + x]), steps + 1]);
 		}
-		visited.add(cellText);
 	}
-	throw new Error(`I couldn't even`);
+	throw new Error(`BFS failed to find graph node meeting end point criteria.`);
 }
 
 function part1(input: string): number {
 	const { start, end, grid } = parseInput(input);
 
-	const steps = bfs(start, end, grid);
-
-	return steps;
+	return bfs(start, end, grid);
 }
 
 function part2(input: string) {
-	const directions = [
-		[0, 1],
-		[0, -1],
-		[1, 0],
-		[-1, 0],
-	];
+	const { start, grid } = parseInput(input);
 
-	const { end, grid, aPoints } = parseInput(input);
-
-	const queue: [string, number][] = aPoints.map((start) =>
-		[cellToString(start), 0] as [string, number]
-	);
-	const visited: Set<string> = new Set();
-
-	while (queue.length > 0) {
-		const [cellText, steps] = queue.shift()!;
-		const [row, col] = stringToCell(cellText);
-		if (visited.has(cellText)) continue;
-		if (row === end[0] && col === end[1]) {
-			return steps;
-		}
-		for (const [y, x] of directions) {
-			if (
-				!getCoordinateWithinBounds(row + y, col + x, grid) ||
-				grid[row + y][col + x] > 1 + grid[row][col] ||
-				visited.has(cellToString([row + y, col + x]))
-			) continue;
-			queue.push([cellToString([row + y, col + x]), steps + 1]);
-		}
-		visited.add(cellText);
-	}
+	return bfs(start, 0, grid);
 }
 
 const test1 = part1(test);
