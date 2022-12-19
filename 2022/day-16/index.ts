f3b2f44b58090fdff3a7fb0c9730b8f375b95e7e
Tom VanAntwerp <tom@tomvanantwerp.com>
2022-12-18 22:06:48 -0500

    Add documentation

--- 2022/day-16/index.ts
+++ 2022/day-16/index.ts
@@ -12,6 +12,14 @@ type Cave = Map<string, Valve>;
 
 type Distances = Map<string, number>;
 
+/**
+ * Takes the raw input for the problem and returns
+ * a Map with keys of valve name (e.g., 'AA') and
+ * values of an object containing the flow rate and
+ * a bitmask representing the valve's position in
+ * the list of all valves (e.g., first valve is
+ * 1, the second 10, the third 100, etc.).
+ */
 function parseInput(input: string): Cave {
 	const cave: Cave = new Map();
 	input.trim().split('\n').forEach((valve, i) => {
@@ -28,6 +36,12 @@ function parseInput(input: string): Cave {
 	return cave;
 }
 
+/**
+ * Uses Floyd-Warshall algorithm to compute distances
+ * between valves. Returns the distances as a Map with
+ * a string key of the from 'AA,BB', where 'AA' and 'BB'
+ * are names of valves, and values of the numeric distance.
+ */
 function getDistances(cave: Cave): Distances {
 	const distances: Distances = new Map();
 	for (const [key1] of cave) {
@@ -56,6 +70,17 @@ function getDistances(cave: Cave): Distances {
 	return distances;
 }
 
+/**
+ * Performs a depth-first search from the starting valve.
+ * Continues while there is time on the clock, and if
+ * the next valve to visit hasn't been opened.
+ * Checks whether a value is opened by performing bitwise
+ * union on the valve's mask and the 'opened' param. Newly
+ * calculated flows are set to the result Map, which is the
+ * eventual returned value. The keys of the result are the
+ * bitwise value representing which valves are open, and the
+ * value is the flow rate.
+ */
 function visit(
 	valve: string,
 	minutes: number,
@@ -65,9 +90,10 @@ function visit(
 	flow: number,
 	result: Map<number, number> = new Map(),
 ) {
-	const n = !result.has(opened) ? 0 : result.get(opened)!;
-	result.set(opened, Math.max(flow, n));
+	const cachedFlow = result.get(opened) ?? 0;
+	result.set(opened, Math.max(flow, cachedFlow));
 	for (const [neighbor, { rate, mask }] of cave) {
+		// Don't waste time on valves you can't open.
 		if (rate === 0) continue;
 		const distance = distances.get(`${valve},${neighbor}`)!;
 		const timeRemaining = minutes - distance - 1;
@@ -85,6 +111,10 @@ function visit(
 
 	return result;
 }
+
+/**
+ * Returns the maximum calculated value after a DFS of the caves.
+ */
 function part1(input: string): number {
 	const cave = parseInput(input);
 	const distances = getDistances(cave);
@@ -93,14 +123,37 @@ function part1(input: string): number {
 	return Math.max(...result.values());
 }
 
+/**
+ * After performing DFS, compares all of the results
+ * with every other result. Whenever there are two
+ * non-overlapping keys, it represents a state that
+ * two actors independently could achieve. If the sum
+ * of those two actors is greater than any seen previously,
+ * set total to that sum.
+ */
 function part2(input: string): number {
-	return 0;
+	const cave = parseInput(input);
+	const distances = getDistances(cave);
+	const result = visit('AA', 26, 0, cave, distances, 0);
+
+	let total = 0;
+	for (const [k1, v1] of result) {
+		for (const [k2, v2] of result) {
+			// Are k1 and k2 non-overlapping states of opened valves?
+			if ((k1 & k2) === 0) {
+				// If yes, update the total if necessary
+				if (v1 + v2 > total) total = v1 + v2;
+			}
+		}
+	}
+
+	return total;
 }
 
 const test1 = part1(test);
 console.assert(test1 === 1651, { expected: 1651, received: test1 });
-// const test2 = part2(test);
-// console.assert(test2 === 93, { expected: 93, received: test2 });
+const test2 = part2(test);
+console.assert(test2 === 1707, { expected: 1707, received: test2 });
 
-console.log(`Part 1: ${part1(input)}`);
-// console.log(`Part 2: ${part2(input)}`);
+// console.log(`Part 1: ${part1(input)}`);
+console.log(`Part 2: ${part2(input)}`);
