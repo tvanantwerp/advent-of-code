86bf4f264cd9cf2320bb364a1cdc478b7783a0cf
Tom VanAntwerp <tom@tomvanantwerp.com>
2023-12-01 10:03:54 -0500

    Finally get project structure right

--- 2023/day01/main.py
+++ 2023/day01/main.py
@@ -1,16 +1,29 @@
 from os import path
+import argparse
 from common.reader import read_input
 
 current_dir = path.dirname(__file__)
+test_path = path.join(current_dir, 'test.txt')
 file_path = path.join(current_dir, 'input.txt')
+puzzle_test_input = read_input(test_path)
 puzzle_input = read_input(file_path)
 
-def part_one():
+def part_one(isTest: bool):
+    inputs = puzzle_test_input if isTest else puzzle_input
+    print(inputs)
+
+
+def part_two(isTest: bool):
     pass
 
-def part_two():
-    pass
+def main():
+    parser = argparse.ArgumentParser(description='Example Script')
+    parser.add_argument('--test', action='store_true', help='Run on test input')
+    args = parser.parse_args()
+    test_mode = "test" if args.test else "full"
+    print(f'Running day one with {test_mode} inputs...')
+    print(part_one(args.test))
+    print(part_two(args.test))
 
 if __name__ == '__main__':
-    print(part_one())
-    print(part_two())
+    main()
