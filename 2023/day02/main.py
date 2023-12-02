f450c7073996bffdbcc61b8a1b08c9f19edd0cc1
Tom VanAntwerp <tom@tomvanantwerp.com>
2023-12-02 10:07:05 -0500

    Solve day 2 part 2

--- 2023/day02/main.py
+++ 2023/day02/main.py
@@ -10,7 +10,7 @@ file_path = path.join(current_dir, "input.txt")
 color_mapping = {"red": 0, "green": 1, "blue": 2}
 
 
-def part_one(inputs: list[str]):
+def parse_games(inputs: list[str]) -> list[list[int]]:
     games: list[list[int]] = []
     for game in inputs:
         game_number, game_counts = game.split(": ")
@@ -26,21 +26,32 @@ def part_one(inputs: list[str]):
                 if int(digit) > max_color_counts[color_mapping[color]]:
                     max_color_counts[color_mapping[color]] = int(digit)
         games.append(max_color_counts)
+    return games
+
+
+def part_one(inputs: list[str]):
+    games = parse_games(inputs)
 
     possible_games = []
-    for game_no, game_max_counts in enumerate(games, start=1):
+    for game_number, game_max_counts in enumerate(games, start=1):
         if (
             game_max_counts[0] <= 12
             and game_max_counts[1] <= 13
             and game_max_counts[2] <= 14
         ):
-            possible_games.append(game_no)
+            possible_games.append(game_number)
 
     return sum(possible_games)
 
 
 def part_two(inputs: list[str]):
-    pass
+    games = parse_games(inputs)
+
+    game_powers = []
+    for game in games:
+        game_powers.append(game[0] * game[1] * game[2])
+
+    return sum(game_powers)
 
 
 def main():
