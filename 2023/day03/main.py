24ad6d9a16d5e74523b787056f44f6e73f7bdf39
Tom VanAntwerp <tom@tomvanantwerp.com>
2023-12-03 09:49:44 -0500

    Setup for day 3

---
+++ 2023/day03/main.py
@@ -0,0 +1,37 @@
+from os import path
+import argparse
+import re
+from common.reader import read_input
+
+current_dir = path.dirname(__file__)
+test_path = path.join(current_dir, "test.txt")
+file_path = path.join(current_dir, "input.txt")
+
+
+def part_one(inputs: list[str]):
+    pass
+
+
+def part_two(inputs: list[str]):
+    pass
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Example Script")
+    parser.add_argument("--test", action="store_true", help="Run on test input")
+    args = parser.parse_args()
+    test_mode = "test" if args.test else "full"
+    print(f"Running day three with {test_mode} inputs...")
+
+    if args.test:
+        puzzle_test_input = read_input(test_path)
+        print(part_one(puzzle_test_input))
+        print(part_two(puzzle_test_input))
+    else:
+        puzzle_input = read_input(file_path)
+        print(part_one(puzzle_input))
+        print(part_two(puzzle_input))
+
+
+if __name__ == "__main__":
+    main()
