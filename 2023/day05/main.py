57f0fd2586141fab1054e62f965f7c055df2e84a
Tom VanAntwerp <tom@tomvanantwerp.com>
2023-12-05 10:57:18 -0500

    Solve part 1

--- 2023/day05/main.py
+++ 2023/day05/main.py
@@ -2,6 +2,7 @@ from os import path
 import argparse
 import re
 from common.reader import read_input
+import sys
 
 current_dir = path.dirname(__file__)
 test_path = path.join(current_dir, "test.txt")
@@ -9,12 +10,12 @@ file_path = path.join(current_dir, "input.txt")
 
 
 def get_mappings(inputs: list[str]):
-    seed_line = inputs.pop(0)
+    seed_line = inputs[0]
     seeds: list[int] = [int(x) for x in re.findall(r"\d+", seed_line)]
-    inputs.pop(0)
-    mappings: list[list[list[int]]] = []
-    current_mapping: list[list[int]] = []
-    for line in inputs:
+    mappings: list[list[tuple[int, int, int]]] = []
+    current_mapping: list[tuple[int, int, int]] = []
+    for i in range(2, len(inputs)):
+        line = inputs[i]
         if line == "":
             mappings.append(current_mapping)
             current_mapping = []
@@ -22,18 +23,46 @@ def get_mappings(inputs: list[str]):
         mapping_values = re.findall(r"\d+", line)
         if len(mapping_values) == 0:
             continue
-        current_mapping.append([int(x) for x in mapping_values])
+        values = [int(x) for x in mapping_values]
+        if len(values) != 3:
+            raise ValueError("Invalid mapping")
+        current_mapping.append((values[0], values[1], values[2]))
     if len(current_mapping):
         mappings.append(current_mapping)
+    for mapping in mappings:
+        mapping.sort()
     return seeds, mappings
+
+
+def get_seed_location(seed: int, mappings: list[list[tuple[int, int, int]]]):
+    for section in mappings:
+        for mapping in section:
+            if mapping[1] + mapping[2] - 1 >= seed >= mapping[1]:
+                seed = mapping[0] + seed - mapping[1]
+                break
+    return seed
+
+
+def get_closest_location(seeds: list[int], mappings: list[list[tuple[int, int, int]]]):
+    closest_location = sys.maxsize
+    for seed in seeds:
+        current_value = get_seed_location(seed, mappings)
+        if current_value < closest_location:
+            closest_location = current_value
+    return closest_location
 
 
 def part_one(inputs: list[str]):
     seeds, mappings = get_mappings(inputs)
-    print(seeds, mappings)
+    closest_location = get_closest_location(seeds, mappings)
+    return closest_location
 
 
 def part_two(inputs: list[str]):
+    seeds, mappings = get_mappings(inputs)
+    seed_pairs = []
+    for i in range(0, len(seeds), 2):
+        seed_pairs.append((seeds[i], seeds[i + 1]))
     pass
 
 
