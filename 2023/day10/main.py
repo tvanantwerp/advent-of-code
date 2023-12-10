3ab7d5f330a511a1731d7a2aa890448889e90faf
Tom VanAntwerp <tom@tomvanantwerp.com>
2023-12-10 12:41:49 -0500

    Solve part 1

--- 2023/day10/main.py
+++ 2023/day10/main.py
@@ -1,69 +1,91 @@
 from os import path
 import argparse
-import re
-from typing import Any
-from math import gcd
 from common.reader import read_input
 
 current_dir = path.dirname(__file__)
 test1_path = path.join(current_dir, "test1.txt")
 test2_path = path.join(current_dir, "test2.txt")
-test3_path = path.join(current_dir, "test3.txt")
 file_path = path.join(current_dir, "input.txt")
+
+directions: dict[str, tuple[tuple[int, int], tuple[int, int]]] = {
+    "|": ((-1, 0), (1, 0)),  # up, down
+    "-": ((0, -1), (0, 1)),  # left, right
+    "L": ((-1, 0), (0, 1)),  # up, right
+    "J": ((-1, 0), (0, -1)),  # up, left
+    "7": ((0, -1), (1, 0)),  # left, down
+    "F": ((0, 1), (1, 0)),  # right, down
+}
 
 
 def parse_inputs(inputs: list[str]):
-    parsed_inputs: dict[str, Any] = {"directions": [], "graph": {}}
-    parsed_inputs["directions"] = [*inputs[0]]
-    for line in inputs[2:]:
-        node_values = re.findall(r"\w+", line)
-        parsed_inputs["graph"][node_values[0]] = {
-            "left": node_values[1],
-            "right": node_values[2],
-        }
-    return parsed_inputs
+    return [[*line] for line in inputs]
+
+
+def get_neighbor(cell: tuple[int, int], direction: tuple[int, int]):
+    return (cell[0] + direction[0], cell[1] + direction[1])
+
+
+def is_valid_cell(grid: list[list[str]], cell: tuple[int, int]):
+    return 0 <= cell[0] < len(grid) and 0 <= cell[1] < len(grid[0])
+
+
+def build_loop(grid: list[list[str]]):
+    # Find the starting point
+    start: tuple[int, int] = (-1, -1)
+    for y, row in enumerate(grid):
+        for x, cell in enumerate(row):
+            if cell == "S":
+                start = (y, x)
+                break
+        if start != (-1, -1):
+            break
+
+    queue = [start]
+    visited = set()
+    while queue:
+        y, x = queue.pop(0)
+        visited.add((y, x))
+        if grid[y][x] == "S":
+            start_directions: list[tuple[int, int]] = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+            for direction in start_directions:
+                neighbor_pipe_coords: tuple[int, int] = get_neighbor((y, x), direction)
+                if is_valid_cell(grid, neighbor_pipe_coords):
+                    neighbor_pipe_type = grid[neighbor_pipe_coords[0]][
+                        neighbor_pipe_coords[1]
+                    ]
+                    if neighbor_pipe_type in directions:
+                        valid_steps = directions[neighbor_pipe_type]
+                        connects_to_start = False
+                        for steps in valid_steps:
+                            neighbor = get_neighbor(neighbor_pipe_coords, steps)
+                            if neighbor == (y, x):
+                                connects_to_start = True
+                                break
+                        if connects_to_start:
+                            queue.append(neighbor_pipe_coords)
+        else:
+            pipe_type = grid[y][x]
+            if pipe_type in directions:
+                valid_directions = directions[pipe_type]
+                neighbor_pipes: list[tuple[int, int]] = []
+                for direction in valid_directions:
+                    new_neighbor = get_neighbor((y, x), direction)
+                    if is_valid_cell(grid, new_neighbor):
+                        neighbor_pipes.append(new_neighbor)
+                for pipe in neighbor_pipes:
+                    if pipe not in visited:
+                        queue.append(pipe)
+
+    return len(visited) // 2
 
 
 def part_one(inputs: list[str]):
     parsed_inputs = parse_inputs(inputs)
-    current_node = "AAA"
-    steps = 0
-    while current_node != "ZZZ":
-        for direction in parsed_inputs["directions"]:
-            steps += 1
-            if direction == "L":
-                current_node = parsed_inputs["graph"][current_node]["left"]
-            elif direction == "R":
-                current_node = parsed_inputs["graph"][current_node]["right"]
-            if current_node == "ZZZ":
-                break
-    return steps
+    return build_loop(parsed_inputs)
 
 
 def part_two(inputs: list[str]):
-    parsed_inputs = parse_inputs(inputs)
-    starting_nodes = []
-    for key in parsed_inputs["graph"].keys():
-        if key.endswith("A"):
-            starting_nodes.append(key)
-    node_steps: dict[str, int] = {}
-    for node in starting_nodes:
-        current_node = node
-        steps = 0
-        while not current_node.endswith("Z"):
-            for direction in parsed_inputs["directions"]:
-                steps += 1
-                if direction == "L":
-                    current_node = parsed_inputs["graph"][current_node]["left"]
-                elif direction == "R":
-                    current_node = parsed_inputs["graph"][current_node]["right"]
-                if current_node.endswith("Z"):
-                    break
-        node_steps[node] = steps
-    steps = 1
-    for s in node_steps.values():
-        steps = steps * s // gcd(steps, s)
-    return steps
+    pass
 
 
 def main():
@@ -71,15 +93,13 @@ def main():
     parser.add_argument("--test", action="store_true", help="Run on test input")
     args = parser.parse_args()
     test_mode = "test" if args.test else "full"
-    print(f"Running day eight with {test_mode} inputs...")
+    print(f"Running day twn with {test_mode} inputs...")
 
     if args.test:
         puzzle_test1_input = read_input(test1_path)
         puzzle_test2_input = read_input(test2_path)
-        puzzle_test3_input = read_input(test3_path)
         print(part_one(puzzle_test1_input))
         print(part_one(puzzle_test2_input))
-        print(part_two(puzzle_test3_input))
     else:
         puzzle_input = read_input(file_path)
         print(part_one(puzzle_input))
