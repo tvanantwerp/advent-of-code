7070c7d1488faf17034a991fa3a32bdc3bb0bbb6
Tom VanAntwerp <tom@tomvanantwerp.com>
2024-12-01 12:14:54 -0500

    Solve day 1 part 2

--- 2024/day-01/index.ts
+++ 2024/day-01/index.ts
@@ -5,12 +5,13 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// const rawInput = readFileSync('input.txt', 'utf-8');
-const rawInput = readFileSync(join(__dirname, 'test.txt'), 'utf-8');
+// const rawInput = readFileSync(join(__dirname, 'test.txt'), 'utf-8');
+const rawInput = readFileSync(join(__dirname, 'input.txt'), 'utf-8');
 const inputLines = rawInput.split('\n');
 
 const list1: number[] = [];
 const list2: number[] = [];
+const dict2: Record<number, number> = {};
 
 const regex = /(-?\d+)\s+(-?\d+)/;
 
@@ -20,9 +21,13 @@ for (let i = 0; i < inputLines.length; i++) {
 		throw new Error('Invalid input');
 	}
 	const pairs = [match[1], match[2]];
-	console.log(pairs);
 	list1.push(+pairs[0]);
 	list2.push(+pairs[1]);
+	if (dict2[+pairs[1]] !== undefined) {
+		dict2[+pairs[1]]++;
+	} else {
+		dict2[+pairs[1]] = 1;
+	}
 }
 
 list1.sort((a, b) => a - b);
@@ -33,9 +38,14 @@ if (list1.length !== list2.length) {
 }
 
 let distances = 0;
+let similarityScore = 0;
 for (let i = 0; i < list1.length; i++) {
 	distances += Math.abs(list1[i] - list2[i]);
+	similarityScore += list1[i] * (dict2[list1[i]] ?? 0);
 }
 
 console.log('Part 1 answer:');
 console.log(distances);
+
+console.log('Part 2 answer:');
+console.log(similarityScore);
