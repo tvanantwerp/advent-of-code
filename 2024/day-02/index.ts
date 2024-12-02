14407b098bd274ff288a7c6b3dedaec0944ef7ae
Tom VanAntwerp <tom@tomvanantwerp.com>
2024-12-02 09:48:33 -0500

    First attempt at part 2

--- 2024/day-02/index.ts
+++ 2024/day-02/index.ts
@@ -11,10 +11,13 @@ const inputLines = rawInput.split('\n');
 
 const records = inputLines.map(line => line.split(' ').map(d => +d));
 
-let safe = 0;
-let unsafe = 0;
-for (const record of records) {
+function checkRecordSafety(
+	record: number[],
+	forgiveness: boolean,
+	secondPass?: boolean,
+): boolean {
 	let recordIsSafe = true;
+	let firstUnsafeIndex: number | undefined;
 	let lastDifference = 0;
 	for (let i = 0; i < record.length - 1; i++) {
 		const nextDifference = record[i] - record[i + 1];
@@ -25,6 +28,7 @@ for (const record of records) {
 				(lastDifference < 0 && record[i] - record[i + 1] > 0))
 		) {
 			recordIsSafe = false;
+			firstUnsafeIndex = i;
 			break;
 		}
 		lastDifference = nextDifference;
@@ -32,16 +36,29 @@ for (const record of records) {
 		const nextDifferenceAbs = Math.abs(nextDifference);
 		if (nextDifferenceAbs < 1 || nextDifferenceAbs > 3) {
 			recordIsSafe = false;
+			firstUnsafeIndex = i;
 			break;
 		}
 	}
 
-	if (recordIsSafe) {
-		safe++;
+	if (forgiveness && firstUnsafeIndex !== undefined && !secondPass) {
+		return checkRecordSafety(record.toSpliced(firstUnsafeIndex, 1), true, true);
 	} else {
-		unsafe++;
+		return recordIsSafe;
 	}
 }
 
+let safe1 = 0;
+let safe2 = 0;
+for (const record of records) {
+	const recordIsSafe1 = checkRecordSafety(record, false);
+	const recordIsSafe2 = checkRecordSafety(record, true);
+
+	if (recordIsSafe1) safe1++;
+	if (recordIsSafe2) safe2++;
+}
+
 console.log('Part 1 answer:');
-console.log(safe);
+console.log(safe1);
+console.log('Part 2 answer:');
+console.log(safe2);
