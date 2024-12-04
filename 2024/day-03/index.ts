c66662f25432d362f2ed0c77a379da38c7eca6f0
Tom VanAntwerp <tom@tomvanantwerp.com>
2024-12-03 21:41:09 -0500

    Solve part 1

--- 2024/day-03/index.ts
+++ 2024/day-03/index.ts
@@ -5,14 +5,21 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const rawInput = readFileSync(join(__dirname, 'test.txt'), 'utf-8');
-// const rawInput = readFileSync(join(__dirname, 'input.txt'), 'utf-8');
+// const rawInput = readFileSync(join(__dirname, 'test.txt'), 'utf-8');
+const rawInput = readFileSync(join(__dirname, 'input.txt'), 'utf-8');
 const inputLines = rawInput.split('\n');
 
-const correctInstructionsRegex = /mul\(\d{1,3},\d{1,3}\)/g;
+const correctInstructionsRegex = /mul\((\d{1,3}),(\d{1,3})\)/g;
 
-const correctInstructions: string[] = [];
+const pairs: [number, number][] = [];
 for (let i = 0; i < inputLines.length; i++) {
-	const matches = inputLines[i].match(correctInstructionsRegex);
-	matches?.forEach(match => correctInstructions.push(match));
+	const matches = inputLines[i].matchAll(correctInstructionsRegex);
+	for (const match of matches) {
+		pairs.push([+match[1], +match[2]]);
+	}
 }
+
+let part1 = pairs.reduce((acc, curr) => acc + curr[0] * curr[1], 0);
+
+console.log('Part 1 answer:');
+console.log(part1);
