2a6c403b24ff042ec2ef818a270834d707a2edbb
Tom VanAntwerp <tom@tomvanantwerp.com>
2024-12-03 21:49:04 -0500

    Solve part 2

--- 2024/day-03/index.ts
+++ 2024/day-03/index.ts
@@ -9,17 +9,26 @@ const __dirname = dirname(__filename);
 const rawInput = readFileSync(join(__dirname, 'input.txt'), 'utf-8');
 const inputLines = rawInput.split('\n');
 
-const correctInstructionsRegex = /mul\((\d{1,3}),(\d{1,3})\)/g;
+const correctInstructionsRegex = /mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)/g;
 
-const pairs: [number, number][] = [];
+const pairs: [number, number, boolean][] = [];
+let doMul = true;
 for (let i = 0; i < inputLines.length; i++) {
 	const matches = inputLines[i].matchAll(correctInstructionsRegex);
 	for (const match of matches) {
-		pairs.push([+match[1], +match[2]]);
+		if (match[0] === 'do()') doMul = true;
+		else if (match[0] === "don't()") doMul = false;
+		else pairs.push([+match[1], +match[2], doMul]);
 	}
 }
 
 let part1 = pairs.reduce((acc, curr) => acc + curr[0] * curr[1], 0);
+let part2 = pairs.reduce(
+	(acc, curr) => (curr[2] ? acc + curr[0] * curr[1] : acc),
+	0,
+);
 
 console.log('Part 1 answer:');
 console.log(part1);
+console.log('Part 2 answer:');
+console.log(part2);
