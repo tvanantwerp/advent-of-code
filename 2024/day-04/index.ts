0e5cad0310c0307675b12b512ec584917f2ff59f
Tom VanAntwerp <tom@tomvanantwerp.com>
2024-12-04 14:43:47 -0500

    Write helper function to navigate grid

--- 2024/day-04/index.ts
+++ 2024/day-04/index.ts
@@ -7,4 +7,57 @@ const __dirname = dirname(__filename);
 
 const rawInput = readFileSync(join(__dirname, 'test.txt'), 'utf-8');
 // const rawInput = readFileSync(join(__dirname, 'input.txt'), 'utf-8');
-const inputLines = rawInput.split('\n');
+const grid = rawInput.split('\n').map(line => line.split(''));
+
+const xMax = grid[0].length;
+const yMax = grid.length;
+
+for (let y = 0; y < yMax; y++) {
+	for (let x = 0; x < xMax; x++) {}
+}
+
+function getNeighborCoordinates(
+	coordinates: [number, number],
+): [number, number][] {
+	const neighbors: [number, number][] = [];
+	const x = coordinates[0];
+	const y = coordinates[1];
+	const upExists = y - 1 >= 0;
+	const downExists = y + 1 <= yMax;
+	const leftExists = x - 1 >= 0;
+	const rightExists = y + 1 <= xMax;
+	// up and left
+	if (upExists && leftExists) {
+		neighbors.push([x - 1, y - 1]);
+	}
+	// up
+	if (upExists) {
+		neighbors.push([x, y - 1]);
+	}
+	// up and right
+	if (upExists && rightExists) {
+		neighbors.push([x + 1, y - 1]);
+	}
+	// left
+	if (leftExists) {
+		neighbors.push([x - 1, y]);
+	}
+	// right
+	if (rightExists) {
+		neighbors.push([x - 1, y - 1]);
+	}
+	// down and left
+	if (downExists && leftExists) {
+		neighbors.push([x - 1, y + 1]);
+	}
+	// down
+	if (downExists) {
+		neighbors.push([x, y + 1]);
+	}
+	// down and right
+	if (downExists && rightExists) {
+		neighbors.push([x + 1, y + 1]);
+	}
+
+	return neighbors;
+}
