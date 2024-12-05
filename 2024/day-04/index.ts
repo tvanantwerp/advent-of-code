c8bc1cf216666a0c956a0f25056f18cc81a62f1c
Tom VanAntwerp <tom@tomvanantwerp.com>
2024-12-04 20:17:22 -0500

    Solve part 2

--- 2024/day-04/index.ts
+++ 2024/day-04/index.ts
@@ -20,20 +20,25 @@ const previousLetter = {
 	M: 'X',
 } as const;
 
-const verifiedGrid: string[][] = Array.from({ length: yMax }, () =>
+// For double-checking my work visually
+const verifiedGrid1: string[][] = Array.from({ length: yMax }, () =>
+	Array.from({ length: xMax }, () => '#'),
+);
+const verifiedGrid2: string[][] = Array.from({ length: yMax }, () =>
 	Array.from({ length: xMax }, () => '#'),
 );
 
 let validXmases = 0;
+let validMasCrosses = 0;
 
-function populateVerifiedGrid(
+function populateVerifiedGrid1(
 	coordinates: [number, number],
 	letter: Xmas,
 	previous?: [number, number],
 ): boolean {
 	const [x, y] = coordinates;
 	if (letter === 'X') {
-		verifiedGrid[y][x] = letter;
+		verifiedGrid1[y][x] = letter;
 		validXmases++;
 		return true;
 	}
@@ -41,20 +46,20 @@ function populateVerifiedGrid(
 	let validSequence = false;
 	const nextInSequence = previousLetter[letter];
 	if (nextInSequence !== undefined) {
-		const neighbors = getNeighborCoordinates(
+		const neighbors = getNeighborCoordinates1(
 			coordinates,
 			nextInSequence,
 			previous,
 		);
 
 		for (let n = 0; n < neighbors.length; n++) {
-			const partOfSequence = populateVerifiedGrid(
+			const partOfSequence = populateVerifiedGrid1(
 				neighbors[n],
 				nextInSequence,
 				coordinates,
 			);
 			if (partOfSequence) {
-				verifiedGrid[y][x] = letter;
+				verifiedGrid1[y][x] = letter;
 				validSequence = true;
 			}
 		}
@@ -62,7 +67,7 @@ function populateVerifiedGrid(
 	return validSequence;
 }
 
-function getNeighborCoordinates(
+function getNeighborCoordinates1(
 	coordinates: [number, number],
 	neighborLetter: Xmas,
 	previous?: [number, number],
@@ -106,13 +111,51 @@ function isValidNeighbor([x, y]: [number, number], letter: Xmas): boolean {
 	return x >= 0 && x < xMax && y >= 0 && y < yMax && grid[y][x] === letter;
 }
 
+function populateVerifiedGrid2(coordinates: [number, number]): boolean {
+	const [x, y] = coordinates;
+	const up = y - 1;
+	const down = y + 1;
+	const left = x - 1;
+	const right = x + 1;
+
+	if (up < 0 || left < 0 || down >= yMax || right >= xMax) return false;
+	const upLeft = grid[up][left];
+	const upRight = grid[up][right];
+	const downLeft = grid[down][left];
+	const downRight = grid[down][right];
+
+	const rotation1 =
+		upLeft === 'M' && upRight === 'M' && downLeft === 'S' && downRight === 'S';
+	const rotation2 =
+		upLeft === 'S' && upRight === 'M' && downLeft === 'S' && downRight === 'M';
+	const rotation3 =
+		upLeft === 'S' && upRight === 'S' && downLeft === 'M' && downRight === 'M';
+	const rotation4 =
+		upLeft === 'M' && upRight === 'S' && downLeft === 'M' && downRight === 'S';
+
+	if (rotation1 || rotation2 || rotation3 || rotation4) {
+		verifiedGrid2[y][x] = grid[y][x];
+		verifiedGrid2[up][left] = upLeft;
+		verifiedGrid2[up][right] = upRight;
+		verifiedGrid2[down][left] = downLeft;
+		verifiedGrid2[down][right] = downRight;
+		validMasCrosses++;
+		return true;
+	}
+
+	return false;
+}
+
 for (let y = 0; y < yMax; y++) {
 	for (let x = 0; x < xMax; x++) {
-		if (grid[y][x] === 'S') populateVerifiedGrid([x, y], grid[y][x] as Xmas);
+		if (grid[y][x] === 'S') populateVerifiedGrid1([x, y], grid[y][x] as Xmas);
+		if (grid[y][x] === 'A') populateVerifiedGrid2([x, y]);
 	}
 }
 
-// console.log(verifiedGrid.map(row => row.join()).join('\n'));
+console.log(verifiedGrid2.map(row => row.join()).join('\n'));
 
 console.log('Part 1 answer:');
 console.log(validXmases);
+console.log('Part 1 answer:');
+console.log(validMasCrosses);
